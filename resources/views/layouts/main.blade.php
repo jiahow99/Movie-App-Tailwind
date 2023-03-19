86f6078ed3677555143d9f47eb3cc32f63cc0397
jiahow <jia_how99@hotmail.com>
2023-03-19 20:44:02 +0800

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie App</title>
+    @vite('resources/css/app.css')
+
+    {{-- FontAwesome icon --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col lg:flex-row items-center">
+                <li>
+                    <a href="#" class="flex items-center">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABKUlEQVR4nO2WTU4CMRSAuzLsvICs2bPQRIY9E+K5HJceA0LCHfhdcwXHYYVGWRrNZxob8lLKoO0UiOFLumhf06/z+qapUqcCUAcGwNq0IdA4hPSFbV51LKZ4wG76Yt4F8AAsgQLI9FiIeC1Et0Ai+u9inhbZZFWJE6At+m9iXuHYXBEiHpakuucQy80FiRumkGxWwNWeVN97i0Vl9/WZmtaTUlFcmfny8OI6CkAXeMafHEh9xDnhPO1Yuw0sgLErWAnWmpfAI/DlikcRA3c6A2UbiyUujW84i33Zl0Fl4206i4/9O/3lAskjXpmJuTJHrmAaKNdp7ahQgNkvZNNgkQ1wA3yadi3Gm8CHObeWqhp+zsT5ehRPn0UM8dSku+aI1YA5MKlcrP4r3zUAtm9D5+gNAAAAAElFTkSuQmCC" alt="movie_app_logo">
+                        <span class="no-underline font-bold ml-2">Movie App</span>
+                    </a> 
+                </li>
+                <li class="lg:ml-16 mt-3 lg:mt-0">
+                    <a href="#" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="lg:ml-6 mt-3 lg:mt-0">
+                    <a href="#" class="hover:text-gray-300">TV Shows</a>
+                </li>
+                <li class="lg:ml-6 mt-3 lg:mt-0">
+                    <a href="#" class="hover:text-gray-300">Actors</a>
+                </li>
+            </ul>
+            <div class="flex flex-col lg:flex-row items-center mt-3 lg:mt-0">
+                <div class="relative">
+                    <i class="fa-solid fa-magnifying-glass text-white absolute mt-2 ml-3 py-auto text-sm"></i>
+                    <input type="text" class="bg-gray-800 rounded-full w-64 pl-10 py-1" placeholder="Search">
+                </div>
+                <div class="ml-4 mt-3 lg:mt-0">
+                    <a href="#" class="">
+                        <i class="fa-solid fa-user rounded-full border-white border-2 p-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+    
+</body>
+</html>
