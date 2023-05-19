cd559459ce92ae59b37d25ca7f0e301a7310d588
Kah How <jia_how99@hotmail.com>
2023-05-19 21:50:50 +0800

    In Progress FIlter data

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -17,11 +17,11 @@ class TvController extends Controller
         // Fetch how many pages of results - 20 result per page
         $max_page = 2;
 
-        $trendingTv = $movieApi->fetchTrending('tv', 2);
+        $trendingTv = $movieApi->fetchTrending('tv', $max_page);
 
-        $popularTv = $movieApi->fetchCategoryTv('popular', 2);
+        $popularTv = $movieApi->fetchCategoryTv('popular', $max_page);
 
-        $topRatedTv = $movieApi->fetchCategoryTv('top_rated', 2);
+        $topRatedTv = $movieApi->fetchCategoryTv('top_rated', $max_page);
 
         $genresList = $movieApi->fetchGenres();
 
@@ -30,5 +30,34 @@ class TvController extends Controller
         $viewModel = new TvsViewModel($trendingTv, $topRatedTv, $popularTv, $genresList, $filterData);
 
         return view('tv.index', $viewModel);
+    }
+
+
+    /**
+     * Fetch Tv by Region
+     */
+    public function tvByRegion(MovieApiService $movieApi, string $region, $page=1)
+    {
+        // Get user input filter data
+        $filterInput = request()->except('region');
+        
+        // Fetch movies by Region
+        $moviesByRegion = $movieApi->fetchRegionMovies($region, $page, $filterInput);
+        
+        // Fetch all genres
+        $genresList = $movieApi->fetchGenres();
+        
+        // Fetch all regions
+        $filterData['regions'] = $movieApi->fetchRegions();
+        
+        // Fetch all years
+        $filterData['years'] = $movieApi->fetchYears();
+
+        $filterType = 'region';
+
+        // View model
+        $viewModel = new CategoryViewModel($region, $moviesByRegion, $genresList, $filterType, $filterData, $filterInput);
+
+        return view('movies.category', $viewModel);
     }
 }
